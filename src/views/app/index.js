4a2834d08f1b542ee460ab82a9954a5e3506effe
luiscelano <luiscelano123@live.com>
2023-08-22 20:57:39 -0600

    feat: added protected routes

---
+++ src/views/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ViewsApp() {
+  console.log('home screen');
+  return <div>Home screen</div>;
+}
+
+export default ViewsApp;
---
+++ src/views/auth/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Auth() {
+  return <div>auth screen</div>;
+}
+
+export default Auth;
