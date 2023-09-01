fde70975d3e3532a2dc40f9a67da271eed329a2b
luiscelano <luiscelano123@live.com>
2023-08-31 23:02:30 -0600

    refactor: removed console logs

--- src/loaders/redirectIfAuthenticated.js
+++ src/loaders/redirectIfAuthenticated.js
@@ -2,7 +2,6 @@ import { redirect } from 'react-router-dom';
 
 function redirectIfAuthenticated() {
   const isAuthenticated = localStorage.getItem('isAuthenticated');
-  console.log('redirect if authenticated', isAuthenticated);
 
   if (!isAuthenticated) return redirect('/auth');
 
